ccd70c2c590277de599e33e6f1b868995cbd608f
Sergey Sit'notyourdeal <63511782+SergaTV@users.noreply.github.com>
2022-04-15 14:05:06 +0300

    Update weather.py

--- modules/weather.py
+++ modules/weather.py
@@ -4,7 +4,7 @@ import logging
 from .. import loader, utils
 import requests
 logger = logging.getLogger(__name__)
-
+__version__ = 2
 @loader.tds
 class WeatherMod(loader.Module):
     """Weather module via wttr.in site"""
@@ -13,11 +13,11 @@ class WeatherMod(loader.Module):
     async def wedcmd(self, message):
         """Sends picture of weather and deletes message"""
         await message.delete()
-        image = (await utils.run_sync(requests.get, f"https://wttr.in/{utils.get_args_raw(message)}_0pq.png")).content
+        image = (await utils.run_sync(requests.get, f"https://wttr.in/{utils.get_args_raw(message)}_0pq_lang=ru.png")).content
         await message.client.send_file(message.to_id, image)
         
     @loader.unrestricted
     async def wecmd(self, message):
         """Sends picture of weather without deleting message"""
-        image = (await utils.run_sync(requests.get, f"https://wttr.in/{utils.get_args_raw(message)}_0pq.png")).content
+        image = (await utils.run_sync(requests.get, f"https://wttr.in/{utils.get_args_raw(message)}_0pq_lang=ru.png")).content
         await message.client.send_file(message.to_id, image)
