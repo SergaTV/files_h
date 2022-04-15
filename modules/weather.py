05ac1d4b910da281e412eb4e29a2345ab3b88179
Sergey Sit'notyourdeal <63511782+SergaTV@users.noreply.github.com>
2022-04-15 09:22:46 +0300

    Create weather.py

---
+++ modules/weather.py
@@ -0,0 +1,23 @@
+#сделал @Aqendo
+import asyncio
+import logging
+from .. import loader, utils
+import requests
+logger = logging.getLogger(__name__)
+
+@loader.tds
+class WeatherMod(loader.Module):
+    """Weather module via wttr.in site"""
+    strings = {"name":"Weather"}
+    @loader.unrestricted
+    async def wedcmd(self, message):
+        """Sends picture of weather and deletes message"""
+        await message.delete()
+        image = (await utils.run_sync(requests.get, f"https://wttr.in/{utils.get_args_raw(message)}_0pq.png")).content
+        await message.client.send_file(message.to_id, image)
+        
+    @loader.unrestricted
+    async def wecmd(self, message):
+        """Sends picture of weather without deleting message"""
+        image = (await utils.run_sync(requests.get, f"https://wttr.in/{utils.get_args_raw(message)}_0pq.png")).content
+        await message.client.send_file(message.to_id, image)
