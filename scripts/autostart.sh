6a48e3b1d6bdf52be6a212566c313bd31e54c8a3
Sergey Sit'notyourdeal <63511782+SergaTV@users.noreply.github.com>
2022-04-24 22:09:53 +0300

    Add files via upload

---
+++ scripts/autostart.sh
@@ -0,0 +1,11 @@
+(cd *riendl* && echo "Description=FTG
+[Service]
+WorkingDirectory=$(pwd)/
+ExecStart=/usr/bin/python3 -m friendly-telegram
+Type=simple
+Restart=always
+RestartSec=1
+User=$(whoami)
+[Install]
+WantedBy=multi-user.target
+" > /etc/systemd/system/ftg.service && systemctl start ftg && systemctl enable ftg) || echo "friendly-telegram not found."
